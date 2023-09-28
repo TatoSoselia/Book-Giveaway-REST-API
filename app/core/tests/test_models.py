a20d1be95ce22a0055de073b947cb18edf50a7ec
TatoSoselia <tatososelia93@gmail.com>
2023-09-28 21:54:52 +0400

    feat: add test for book model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,8 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
 
 class ModelTests(TestCase):
     """Test models."""
@@ -46,3 +48,20 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_book_successful(self):
+        """Test create e book is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        book = models.Book.objects.create(
+            user = user,
+            title = 'test title',
+            author = 'test author',
+            description='sample book description',
+            available = True,
+            pickup_location = 'test location',
+        )
+
+        self.assertEqual(str(book), book.title)
