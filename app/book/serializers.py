5ae14527fbf9539b66790a34df7e33a8c738afa8
TatoSoselia <tatososelia93@gmail.com>
2023-09-29 10:52:45 +0400

    feat: implement the book listing API

---
+++ app/book/serializers.py
@@ -0,0 +1,18 @@
+"""
+Serializers for book APIs
+"""
+from rest_framework import serializers
+
+from core.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    """Serializer for books."""
+
+    class Meta:
+        model = Book
+        fields = [
+            'id', 'title', 'author', 'description',
+            'available', 'pickup_location',
+        ]
+        read_only_fields = ['id']
--- app/book/views.py
+++ app/book/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""
+Views for the book APIs
+"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from core.models import Book
+from book import serializers
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    """View for manage book APIs."""
+    serializer_class = serializers.BookSerializer
+    queryset = Book.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return self.queryset.filter(user=self.request.user).order_by('-id')
+        else:
+            return self.queryset.filter(available=True).order_by('-id')
