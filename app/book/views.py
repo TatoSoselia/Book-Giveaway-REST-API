4a54564e42346834dd2ee4cd5b07ad3e2b5634c4
TatoSoselia <tatososelia93@gmail.com>
2023-09-30 11:57:40 +0400

    fix: prevent authorized user's unintended access to another user data (modification/deletion)

--- app/book/views.py
+++ app/book/views.py
@@ -1,7 +1,7 @@
 """
 Views for the book APIs
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.generics import ListAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import (
@@ -12,12 +12,24 @@ from core.models import Book
 from book import serializers
 
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+
 class BookViewSet(viewsets.ModelViewSet):
     """View for manage book APIs."""
     serializer_class = serializers.BookSerializer
     queryset = Book.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return super().get_permissions()
 
     def get_queryset(self):
         return self.queryset.filter(available=True).order_by('-id')
